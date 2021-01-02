e6acc5b0e73f86f7f2dc0d7422248b7ed488c99d
Bherly Novrandy <pushm0v.development@gmail.com>
2021-01-03 06:07:44 +0700

    Add http server & handler

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+type server struct{}
+
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "hello world"}`))
+}
+
+func main() {
+	var s = &server{}
+	http.Handle("/", s)
+	log.Println("Running http server")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
