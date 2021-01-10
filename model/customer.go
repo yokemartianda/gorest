f46467b53877bc8e35425ba0ae3af007734097a1
yokemartianda <yokemartianda@gmail.com>
2021-01-10 23:30:35 +0700

    add soft delete functionality

--- model/customer.go
+++ model/customer.go
@@ -1,7 +1,16 @@
 package model
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type Customer struct {
-	Id int `json:"id" gorm:"autoIncrement;primaryKey"`
-	Name string `json:"name"`
-	Address string `json:"address"`
+	Id        int            `json:"id" gorm:"autoIncrement;primaryKey"`
+	Name      string         `json:"name"`
+	Address   string         `json:"address"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index"`
 }
